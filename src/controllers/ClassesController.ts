07d51f5b922c295ea585659a4218babde42915de
Taynan Souza <taynan.tws@gmail.com>
2020-08-09 04:15:19 -0300

    Route to list all available classes
    The route will list all teachers/classes whitin a time, week day and subject.

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -10,6 +10,47 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    if  (!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: 'Missing filter to search classes'
+      });
+    }
+
+    try {
+      const week_day = filters.week_day as string;
+      const subject = filters.subject as string;
+      const time = filters.time as string;
+
+      const timeInMinutes = ConvertHourToMinutes(time);
+      console.log(timeInMinutes);
+
+      const classes = await db('classes')
+        .whereExists(function () {
+          this.select('class_schedule.*')
+            .from('class_schedule')
+            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+            .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'users.id')
+        .select(['classes.*', 'users.*']);
+
+      return response.json(classes);
+
+    } catch (error) {
+      console.log(new Date(), error);
+
+      return response.status(400).json({
+        error: 'Unexpected error while listing classes'
+      })
+    }
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
@@ -54,6 +95,7 @@ export default class ClassesController {
 
       return response.status(201).json({});
     } catch (error) {
+      console.log(new Date(), error);
       await trx.rollback();
 
       return response.status(400).json({
